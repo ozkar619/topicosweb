36bbdf2f0ef4f70bb7403970d729e8e3fbf2d8b7
Oscar Grimaldo Aguayo <oscar.grimaldo@itcelaya.edu.mx>
2015-02-18 17:48:04 -0600

    archivo git ignore

--- 06030450/revista/protected/views/layouts/main.php
+++ 06030450/revista/protected/views/layouts/main.php
@@ -81,28 +81,21 @@
       <div class="row">
         <div class="col-sm-3 col-md-2 sidebar">
           <ul class="nav nav-sidebar">
+
             <li class="active"><a href="#">Overview <span class="sr-only">(current)</span></a></li>
-            <li><a href="#">Artic</a></li>
-            <li><a href="#">Analytics</a></li>
-            <li><a href="#">Export</a></li>
+            <li><a href="?r=articulo/agregar">Articulos</a></li>
+            <li><a href="?r=revista/admin">Revistas</a></li>
+            <li><a href="?r=autor/admin">Autores</a></li>
+            
           </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nav item</a></li>
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-            <li><a href="">More navigation</a></li>
-          </ul>
-          <ul class="nav nav-sidebar">
-            <li><a href="">Nav item again</a></li>
-            <li><a href="">One more nav</a></li>
-            <li><a href="">Another nav item</a></li>
-          </ul>
+
+  
         </div>
         <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
-          <h1 class="page-header"><?php echo CHtml::encode(Yii::app()->name); ?></h1>
-          <?php echo $content; ?>
-          <h2 class="sub-header">Section title</h2>
+          <h1 class="page-header text-center"><?php echo CHtml::encode(Yii::app()->name); ?></h1>
+          
+            <?php echo $content; ?>
+          
           <div class="table-responsive">
         
           </div>
