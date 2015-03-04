4c7c335cab4ea7d1a5de7860a2bddf63c5aca4cb
Hugo <hugolv_speed@hotmail.com>
2015-03-04 17:56:11 -0600

    Soy Hugo

---
+++ 11030906/revista/protected/views/layouts/column2.php
@@ -0,0 +1,19 @@
+<?php /* @var $this Controller */ ?>
+<?php $this->beginContent('//layouts/main'); ?>
+
+<div>
+		<?php echo $content; ?>
+</div>
+<div>
+	<?php
+		$this->beginWidget('zii.widgets.CPortlet', array(
+			'title'=>'Operations',
+		));
+		$this->widget('zii.widgets.CMenu', array(
+			'items'=>$this->menu,
+			'htmlOptions'=>array('class'=>'operations'),
+		));
+		$this->endWidget();
+	?>
+</div>
+<?php $this->endContent(); ?>
