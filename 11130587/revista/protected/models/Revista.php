c19174e9a430521ee7b864f3762f004f7a615a60
YeseniaME <yesenia-me@hotmail.com>
2015-03-04 21:57:39 -0600

    ImagenLista

--- 11130587/revista/protected/models/Revista.php
+++ 11130587/revista/protected/models/Revista.php
@@ -44,7 +44,7 @@ class Revista extends CActiveRecord
 			array('portada','file', 'types'=>'jpg,gif,png'),
 			array('numero, id_status', 'numerical', 'integerOnly'=>true),
 			array('nombre', 'length', 'max'=>500),
-			//array('portada', 'length', 'max'=>100),
+			array('portada', 'length', 'max'=>100),
 			array('volumen, clave', 'length', 'max'=>45),
 			array('titulo, subtitulo', 'length', 'max'=>200),
 			array('directorio, editorial', 'safe'),
