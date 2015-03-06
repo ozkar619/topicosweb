45ff45e8607df4b5c68d46e0382bfd71d1c16a30
YeseniaME <yesenia-me@hotmail.com>
2015-03-06 16:13:35 -0600

    prob

--- 11130587/revista/protected/views/site/_revista.php
+++ 11130587/revista/protected/views/site/_revista.php
@@ -1,30 +1,18 @@
-    <div class="container">
-      <div class="row row-offcanvas row-offcanvas-right">
-        <div class="col-xs-12 col-sm-9">
-          <p class="pull-right visible-xs">
-            <button type="button" class="btn btn-primary btn-xs" data-toggle="offcanvas">Toggle nav</button>
-          </p>
-          <div class="jumbotron">
-            <h1>Revistas</h1>
-            <p>Aquí puede encontrar un listado de las revistas existentes.</p>
-          </div>
-          <div class="row">
-            <div class="col-xs-6 col-lg-4">
-              <h2><?php echo $data->titulo; ?></h2>
-              <p>
-              <?php echo $data->subtitulo; ?>
-              </p>
-              <p><a class="btn btn-default" href="#" role="button">Detalles &raquo;</a></p>
-            </div><!--/.col-xs-6.col-lg-4-->
-          </div><!--/row-->
-        </div><!--/.col-xs-12.col-sm-9-->
-        </div><!--/.sidebar-offcanvas-->
-      </div><!--/row-->
-
-      <hr>
-
-      <footer>
-        <p>&copy; Company 2014</p>
-      </footer>
-
-    </div><!--/.container-->
+<div class="media">
+  <div class="media-left media-top">
+    <a href="#">
+      <img class="media-object" 
+      src=<?php echo "img/" . $data->portada; ?> height="100" width="100" alt="...">
+    </a>
+  </div>
+  <div class="media-body">
+    <h4 class="media-heading"><?php echo "Nombre: " . $data->nombre; ?></h4>
+    <p><?php echo "Titúlo: " . $data->titulo; ?></p>
+    <p><?php echo "Subtitulo: " . $data->subtitulo; ?></p>
+    <p><?php echo "Editorial: " . $data->editorial; ?></p>
+    <p><?php echo "Volumen: " . $data->volumen; ?></p>
+    <p><?php echo "Número: " . $data->numero; ?></p>
+    <p><?php echo "Fecha: " . $data->fecha; ?></p>
+    <p><?php echo "Clave: " . $data->clave; ?></p>
+  </div>
+</div>   
